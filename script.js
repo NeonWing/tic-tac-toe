318148e53f2814e077f1e6e49a034a9ab85cc69f
neonwing <neonwing3@gmail.com>
2021-12-05 08:34:23 -0500

    Finished God system set up and added wrench buttons to cards

--- script.js
+++ script.js
@@ -60,18 +60,91 @@ const gameManager = (() => {
     }
 })(); 
 
+const gameDisplay = (() => {
+    const playerOneCard = document.querySelector(".player-one");
+    const playerTwoCard = document.querySelector(".player-two");
+    const vs = document.querySelector(".vs");
+    let colorMode = "light";
+
+    const changeToGameBoard = () => {
+        if(playerOne.type === undefined || playerTwo.type === undefined) return;
+
+        playButton.addEventListener("animationend", function() {
+            document.querySelector(".player-select-section").classList.add("hidden");
+            document.querySelector(".play-button-section").classList.add("hidden");
+            document.querySelector(".screen-two").classList.remove("hidden");
+            document.querySelector(".screen-two").classList.add("animate__animated", "animate__fadeIn");
+        });
+
+        playerOneCard.classList.add("animate__animated", "animate__fadeOutLeft");
+        playerTwoCard.classList.add("animate__animated", "animate__fadeOutRight"); 
+        vs.classList.add("animate__animated", "animate__fadeOutDown");
+        playButton.classList.add("animate__animated", "animate__fadeOutDown");       
+    }
+
+    const showCurrentTurn = () => {
+        const playerOneNameTag = document.querySelector(".player-one-name");
+        const playerTwoNameTag = document.querySelector(".player-two-name");
+
+        playerTwoNameTag.classList.toggle("active");
+        playerOneNameTag.classList.toggle("active");
+    }
+
+    const playButton = document.querySelector(".play-button");
+    playButton.addEventListener('click', changeToGameBoard);
+
+    const colorModeSwitcher = document.getElementById("colorModeSwitch");
+    colorModeSwitcher.checked = false;
+    colorModeSwitcher.addEventListener('change', () => {
+        (gameDisplay.colorMode === "light") ? gameDisplay.colorMode = "dark" : gameDisplay.colorMode = "light";
+        console.log(gameDisplay.colorMode);
+        let elements = [...document.querySelectorAll(".light-back, .light-text, .light-button, .light-button-text, .box-light , .marked-light, .dark-back, .dark-text, .dark-button, .dark-button-text, .box-dark, .marked-dark")];
+        elements.forEach((el) => {
+            if(el.classList.contains("light-back") || el.classList.contains("dark-back")) {
+                el.classList.toggle("light-back");
+                el.classList.toggle("dark-back");
+            }
+            if(el.classList.contains("light-text") || el.classList.contains("dark-text")) {
+                el.classList.toggle("light-text");
+                el.classList.toggle("dark-text");
+            }
+            if(el.classList.contains("light-button") || el.classList.contains("dark-button")) {
+                el.classList.toggle("light-button");
+                el.classList.toggle("dark-button");
+            }
+            if(el.classList.contains("light-button-text") || el.classList.contains("dark-button-text")) {
+                el.classList.toggle("light-button-text");
+                el.classList.toggle("dark-button-text");
+            }
+            if(el.classList.contains("box-light") || el.classList.contains("box-dark")) {
+                el.classList.toggle("box-light");
+                el.classList.toggle("box-dark");
+            }
+            if(el.classList.contains("marked-light") || el.classList.contains("marked-dark")) {
+                el.classList.toggle("marked-light");
+                el.classList.toggle("marked-dark");
+            }
+        });
+    });
+
+    return {
+        showCurrentTurn,
+        colorMode
+    }
+})();
+
 const gameBoard = (() => {
     const boxes = [];
     const board = document.querySelector(".board");
     let enabled = true;
 
     const markSpot = function(e) {
-        if(e.target.classList.contains("marked")) return;
+        if(e.target.classList.contains(`marked-${gameDisplay.colorMode}`)) return;
         if(gameBoard.enabled) {
             if(e.target.classList.contains("gray")) {
                 e.target.classList.remove("gray");
             }
-            e.target.classList.add("marked");
+            e.target.classList.add(`marked-${gameDisplay.colorMode}`);
 
             gameManager.currentPlayer.spots.push(boxes.indexOf(e.target));
 
@@ -87,14 +160,14 @@ const gameBoard = (() => {
     }
 
     const highlightSpot = function(box, symbol) {
-        if(box.classList.contains("marked")) return;
+        if(box.classList.contains(`marked-${gameDisplay.colorMode}`)) return;
 
         box.textContent = symbol;
         box.classList.add("gray");
     }
 
     const clearSpot = function(e) {
-        if(e.target.classList.contains("marked")) return;
+        if(e.target.classList.contains(`marked-${gameDisplay.colorMode}`)) return;
 
         e.target.textContent = "";
         e.target.classList.remove("gray");
@@ -104,6 +177,7 @@ const gameBoard = (() => {
     for(let i = 0; i < 9; i++) {
         boxes.push(document.createElement('div'));
         boxes[i].classList.add("box");
+        (gameDisplay.colorMode === "light") ? boxes[i].classList.add("box-light") : boxes[i].classList.add("box-dark");
         boxes[i].addEventListener('click', markSpot);
         boxes[i].addEventListener('mouseover', () => {
             highlightSpot(boxes[i], gameManager.currentPlayer.symbol);
@@ -128,9 +202,12 @@ const playerSelectManager = (() => {
     const setPlayerType = function(button, player, dataRef) {
         if (playerOne.type != null && player === "one" || playerTwo.type != null && player === "two") {
             let spread = [...document.querySelector(`.player-${player}-select`).children];
+            console.log(spread);
             spread.forEach((button) => {
-                if(button.classList.contains("selected-player")) {
-                    button.classList.remove("selected-player");
+                if(button.classList.contains(playerOne.god.buttonClass) || button.classList.contains(playerTwo.god.buttonClass)) {
+                    button.classList.remove(playerTwo.god.buttonClass);
+                    button.classList.remove(playerOne.god.buttonClass);
+                    
                 } else if (button.classList.contains("selected-ai")) {
                     button.classList.remove("selected-ai");
                 }
@@ -166,67 +243,6 @@ const playerSelectManager = (() => {
     });
 })();
 
-const gameDisplay = (() => {
-    const playerOneCard = document.querySelector(".player-one");
-    const playerTwoCard = document.querySelector(".player-two");
-    const vs = document.querySelector(".vs");
-    let colorMode = "light";
 
-    const changeToGameBoard = () => {
-        if(playerOne.type === undefined || playerTwo.type === undefined) return;
-
-        playButton.addEventListener("animationend", function() {
-            document.querySelector(".player-select-section").classList.add("hidden");
-            document.querySelector(".play-button-section").classList.add("hidden");
-            document.querySelector(".screen-two").classList.remove("hidden");
-            document.querySelector(".screen-two").classList.add("animate__animated", "animate__fadeIn");
-        });
-
-        playerOneCard.classList.add("animate__animated", "animate__fadeOutLeft");
-        playerTwoCard.classList.add("animate__animated", "animate__fadeOutRight"); 
-        vs.classList.add("animate__animated", "animate__fadeOutDown");
-        playButton.classList.add("animate__animated", "animate__fadeOutDown");       
-    }
-
-    const showCurrentTurn = () => {
-        const playerOneNameTag = document.querySelector(".player-one-name");
-        const playerTwoNameTag = document.querySelector(".player-two-name");
-
-        playerTwoNameTag.classList.toggle("active");
-        playerOneNameTag.classList.toggle("active");
-    }
-
-    const playButton = document.querySelector(".play-button");
-    playButton.addEventListener('click', changeToGameBoard);
-
-    const colorModeSwitcher = document.getElementById("colorModeSwitch");
-    colorModeSwitcher.checked=false;
-    colorModeSwitcher.addEventListener('change', () => {
-        let elements = [...document.querySelectorAll(".light-back, .light-text, .light-button, .light-button-text, .dark-back, .dark-text, .dark-button, .dark-button-text")];
-        elements.forEach((el) => {
-            if(el.classList.contains("light-back") || el.classList.contains("dark-back")) {
-                el.classList.toggle("light-back");
-                el.classList.toggle("dark-back");
-            }
-            if(el.classList.contains("light-text") || el.classList.contains("dark-text")) {
-                el.classList.toggle("light-text");
-                el.classList.toggle("dark-text");
-            }
-            if(el.classList.contains("light-button") || el.classList.contains("dark-button")) {
-                el.classList.toggle("light-button");
-                el.classList.toggle("dark-button");
-            }
-            if(el.classList.contains("light-button-text") || el.classList.contains("dark-button-text")) {
-                el.classList.toggle("light-button-text");
-                el.classList.toggle("dark-button-text");
-            }
-        });
-    });
-
-    return {
-        showCurrentTurn,
-        colorMode
-    }
-})();
 
 
